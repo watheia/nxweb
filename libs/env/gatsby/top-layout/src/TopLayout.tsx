b7c1c56d2d3d4b38c5760a14935ac45896de477b
Aaron R Miller <amiller@watheia.org>
2021-10-03 13:19:14 -0700

    make global styles component buildable

--- libs/env/gatsby/top-layout/src/TopLayout.tsx
+++ libs/env/gatsby/top-layout/src/TopLayout.tsx
@@ -2,6 +2,7 @@ import PropTypes from "prop-types"
 import { Helmet } from "react-helmet"
 import { ThemeProvider } from "@watheia/theme.provider"
 
+// eslint-disable-next-line @nrwl/nx/enforce-module-boundaries
 import "@watheia/theme.styles.global/index.css"
 import SEO from "./SEO"
 import { ReactNode } from "react"
@@ -16,7 +17,7 @@ export function TopLayout(props: TopLayoutProps) {
       <Helmet>
         <meta name="viewport" content="initial-scale=1, width=device-width" />
         <link
-          href="https://fonts.googleapis.com/css?family=Roboto:400,500,700&display=swap"
+          href="https://fonts.googleapis.com/css?family=Roboto:400,500,700&amp;display=swap"
           rel="stylesheet"
         />
         <SEO />
